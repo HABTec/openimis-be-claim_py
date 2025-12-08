06660812cb5e2f057cd1b709075d1caf40cf45b5
Dawit Yimer <dawityimer52@gmail.com>
2025-12-08 14:08:55 +0300

    fix: name of claim return reasons

--- claim/apps.py
+++ claim/apps.py
@@ -51,22 +51,24 @@ DEFAULT_CFG = {
     "claim_max_restore": None,
     "allowed_domains_attachments": [],
     "verify_quantities": False,
-    "return_reasons":[{
-        "code": 1,
-        "name": "Invalid Item or Service"
-    },{
-        "code": 2,
-        "name": "Not in Price List"
-    },{
-        "code": 3,
-        "name": "No Product Found"
-    },{
-        "code": 4,
-        "name": "Category Limitation"
-    },{
-        "code": 5,
-        "name": "Frequency Failure"
-    }]
+    "return_reasons":[
+        {
+            "code": 1,
+            "name": "Missing Documents"
+        },
+        {
+            "code": 2,
+            "name": "Incorrect Diagnosis Code"
+        },
+        {
+            "code": 3,
+            "name": "Service or Item mismatch"
+        },
+        {
+            "code": 4,
+            "name": "Insufficient Details"
+        }
+    ]
 
 }
 
@@ -124,8 +126,7 @@ class ClaimConfig(AppConfig):
     # Provide absolute path to autogenerating function for claim code
     autogenerate_func = None
     additional_diagnosis_number_allowed = None  # Currently code supports 4 diagnoses maximum, going above will not work
-    allowed_domains_attachments = None,
-    
+    allowed_domains_attachments = None
     return_reasons = []
 
     def __load_config(self, cfg):
